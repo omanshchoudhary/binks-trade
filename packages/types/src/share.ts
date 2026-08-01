37e26cbbb187616ffca1e0cfb6cc2dc57e3c56e8
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-08-01 23:23:33 +0530

    feat(types): add shared share type

---
+++ packages/types/src/share.ts
@@ -0,0 +1,12 @@
+export type Share = {
+    id: string;
+    symbol: string;
+    name: string;
+    sector: string;
+    price: number;
+    previousClose: number;
+    week52High: number;
+    week52Low: number;
+    changePercent: number;
+    priceUpdatedAt: string;
+};
